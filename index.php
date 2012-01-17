94dbac4c29bcc1aaaf0e44f4a65b64300937598a
Arielle Nguyen <ariellephan@gmail.com>
2012-01-17 02:39:32 -0800

    First feed successfully parsed

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php include('includes/header.php'); ?> 
+<body>
+ <div data-role="page">
+   <header data-role="header" class="snsd">
+     <h1>
+       <img src="img/bof.png" alt="Tuts+" />
+     </h1>
+   </header>
+   <!-- /header -->
+
+   <div data-role="content">
+     <ul data-role="listview" data-theme="c" data-dividertheme="d" data-counttheme="e">
+       <li>
+         <a href="site.php?siteName=Bof-TheBusinessOfFashionIntelligence"> Intelligence </a>
+       </li> 
+       <li>
+         <a href="site.php?siteName=Bof-TheBusinessOfFashionInsightAnalysis"> Insight and Analysis </a>
+       </li>
+     </ul>
+   </div>
+
+   <footer data-role="footer" class="snsd">
+      <h4> www.businessoffashion.com </h4>
+   </footer>
+
+ </div>
+ <!-- /page -->
+</body>
+</html>
+
---
+++ site.php
@@ -0,0 +1,51 @@
+<?php
+// If "siteName" isn't in the querystring, set the default site name to 'nettuts'
+$siteName = empty($_GET['siteName']) ? 'nettuts' : $_GET['siteName'];
+
+$siteList = array(
+   'Bof-TheBusinessOfFashionIntelligence',
+	'Bof-TheBusinessOfFashionInsightAnalysis'
+
+);
+
+// For security reasons. If the string isn't a site name, just change to 
+// nettuts instead.
+if ( !in_array($siteName, $siteList) ) {
+   $siteName = 'Bof-TheBusinessOfFashionInsightAnalysis';
+}
+
+$cache = dirname(__FILE__) . "/cache/$siteName";
+// Re-cache every three hours
+if(filemtime($cache) < (time() - 10800))
+{
+   // Get from server
+   if ( !file_exists(dirname(__FILE__) . '/cache') ) {
+      mkdir(dirname(__FILE__) . '/cache', 0777);
+   }
+   // YQL query 
+   $path = "http://query.yahooapis.com/v1/public/yql?q=";
+   $path .= urlencode("SELECT * FROM feed WHERE url='http://feeds.feedburner.com/$siteName'");
+   $path .= "&format=json";
+
+   // Call YQL, and if the query didn't fail, cache the returned data
+   $feed = file_get_contents($path, true);
+
+   // If something was returned, cache
+   if ( is_object($feed) && $feed->query->count ) {
+      $cachefile = fopen($cache, 'wb');
+      fwrite($cachefile, $feed);
+      fclose($cachefile);
+   }
+}
+else
+{
+   // We already have local cache. Use that instead.
+   $feed = file_get_contents($cache);
+}
+
+// Decode feeds
+$feed = json_decode($feed);
+
+// Include the view
+include('views/site.tmpl.php');
+
